8dd9f4cda0f425c88879fab8fe0fa4ea0da7ac5a
heima <598214297.qq.com>
2018-09-17 22:13:45 +0800

    Initial commit

---
+++ app/src/main/java/com/heima/testjar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.heima.testjar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.heima.buildjar.Test;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ buildjar/src/main/java/com/heima/buildjar/Test.java
@@ -0,0 +1,10 @@
+package com.heima.buildjar;
+
+import android.util.Log;
+
+public class Test {
+
+    public static void showLog(){
+        Log.e("Test","Use jar success~");
+    }
+}
